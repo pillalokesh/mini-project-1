54f68761b31fc16f567b4d796cfdaba8441d1c4f
pillalokesh <pillalokesh3@gmail.com>
2026-02-26 22:38:46 +0530

    edit backendfile

--- terraform-aws-mini-project/backend.tf
+++ terraform-aws-mini-project/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "lokesh-terraform-state-2026"
+    bucket  = "lokeshproject1234"
     key     = "mini-project/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
